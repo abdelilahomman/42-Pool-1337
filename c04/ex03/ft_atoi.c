7c1113587dcb23da4db08a284d17e2661a0f3796
abdelilahomman <82535631+abdelilahomman@users.noreply.github.com>
2021-07-07 21:13:22 +0100

    Update ft_atoi.c

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -14,27 +14,28 @@ int	ft_atoi(char *str)
 
 {
 	unsigned int	i;
-	unsigned int	j;
 	unsigned int	n;
+	unsigned int	sin;
 
 	i = 0;
-	j = 0;
 	n = 0;
-	while ((str[i] != '\0')
-		|| (str[i] == 32)
-		|| (str[i] == '+')
-		|| (str[i] >= '0' && str[i] <= '9')
-		|| (str[i] == '-'))
+	sin = 0;
+	while ((str[i] >= 9 && str[i] <= 13) || (str[i] == 32))
+		i++;
+	while ((str[i] == '-') || (str[i] == '+'))
 	{
 		if (str[i] == '-')
-			j++;
-		if (str[i] >= '0' && str[i] <= '9')
-			n = n * 10 + (str[i] - '0');
+			sin++;
 		i++;
 	}
-	if (j % 2 == 1)
-		j = -1;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		n = n * 10 + (str[i] - 48);
+		i++;
+	}
+	if (sin % 2 == 1)
+		sin = -1;
 	else
-		j = 1;
-	return (n * j);
+		sin = 1;
+	return (n * sin);
 }
